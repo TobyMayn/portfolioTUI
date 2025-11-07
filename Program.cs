6e8d116d9dcc2c384537f908682e60d1a39d927d
Tobias Mayn <Tobiasmayn@gmail.com>
2025-11-07 16:34:25 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using portfolioTUI;
+using Terminal.Gui;
+using Terminal.Gui.App;
+using Terminal.Gui.Drawing;
+using Terminal.Gui.Input;
+using Terminal.Gui.ViewBase;
+using Terminal.Gui.Views;
+
+Application.Init();
+
+try
+{
+    Application.Run(new MyView());
+}
+finally
+{
+    Application.Shutdown();
+}
