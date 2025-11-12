ffcc26afbf90e174a97fe617b9c656fe18cc8554
TobyMayn <tobiasmayn@gmail.com>
2025-11-12 14:33:43 +0100

    Add App Bar test

--- SuperView.cs
+++ SuperView.cs
@@ -14,21 +14,38 @@ namespace portfolioTUI {
     public class SuperView : Terminal.Gui.Views.Window {
         LandingPageView landingPage = new ();
         AboutPageView aboutPage = new ();
+        private Bar appBar;
 
+        Shortcut short1 = new Shortcut();
         public SuperView() {
             this.KeyBindings.Clear();
+            
             this.Width = Dim.Fill(0);
             this.Height = Dim.Fill(0);
             this.X = 0;
             this.Y = 0;
             this.Visible = true;
-            this.Arrangement = Terminal.Gui.ViewBase.ViewArrangement.Overlapped;
+            this.Arrangement = Terminal.Gui.ViewBase.ViewArrangement.Fixed;
             this.CanFocus = true;
             this.ShadowStyle = Terminal.Gui.ViewBase.ShadowStyle.None;
             this.Modal = false;
             this.TextAlignment = Terminal.Gui.ViewBase.Alignment.Start;
             this.Title = "Portfolio TUI";
-            this.Add(landingPage);
+            //this.Add(landingPage);
+
+            this.appBar = new Bar();
+            this.appBar.Width = Dim.Auto();
+            this.appBar.Height = Dim.Auto();
+            this.appBar.X = Pos.Center();
+            this.appBar.Y = Pos.Top(this);
+            this.appBar.Visible = true;
+            this.appBar.Arrangement = Terminal.Gui.ViewBase.ViewArrangement.Fixed;
+            this.appBar.CanFocus = true;
+            this.appBar.ShadowStyle = Terminal.Gui.ViewBase.ShadowStyle.None;
+            this.short1.Title = "testing";
+            this.short1.Visible = true;
+            this.appBar.AddShortcutAt(0,this.short1);
+            this.Add(this.appBar);
 
             //  Toggle visibility of Label on keypress
             this.AddCommand(Command.Accept, () => 
