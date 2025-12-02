361ea36dd0a1b34937a3529393faa954705cfce0
TobyMayn <tobiasmayn@gmail.com>
2025-12-02 19:38:01 +0100

    make contact page focusable to make writing in the textfields possible

--- views/pages/ContactPage.Designer.cs
+++ views/pages/ContactPage.Designer.cs
@@ -15,13 +15,6 @@ namespace portfolioTUI {
         
         
         private Label label;
-        private Label name;
-        private Label email;
-        private Label message;
-        private TextField nameInput;
-        private TextField emailInput;
-        private TextField messageInput;
-        private Button submitButton;
         
         private bool visible = true;    
 
@@ -44,71 +37,73 @@ namespace portfolioTUI {
             this.label.Data = "label";
             this.label.Text = "Contact Me";
             this.label.TextAlignment = Terminal.Gui.ViewBase.Alignment.Start;
-            this.name = new Label 
+            var name = new Label 
             { 
                 Text = "Name: ",
                 X = Pos.Left(this.label)-10,
                 Y = Pos.Bottom(this.label) + 1, 
             };
-            this.nameInput = new TextField 
+            var nameInput = new TextField 
             {
-                X = Pos.Right(this.name) + 1,
+                X = Pos.Right(name) + 1,
                 Y = Pos.Bottom(this.label) + 1,
                 Width = Dim.Fill()-30, 
             };
 
-            this.email = new Label
+            var email = new Label
             { 
                 Text = "Email: ",
-                X = Pos.Left(this.name),
-                Y = Pos.Bottom(this.name) +1
+                X = Pos.Left(name),
+                Y = Pos.Bottom(name) +1
             };
-            this.emailInput = new TextField
+            var emailInput = new TextField
             {
-                X = Pos.Right(this.email) + 1,
-                Y = Pos.Bottom(this.name) + 1,
+                X = Pos.Right(email) + 1,
+                Y = Pos.Bottom(name) + 1,
                 Width = Dim.Fill() - 30, 
             };
 
-            this.message = new Label
+            var message = new Label
             { 
                 Text = "Message: ",
-                X = Pos.Left(this.email),
-                Y = Pos.Bottom(this.email) + 1 
+                X = Pos.Left(email),
+                Y = Pos.Bottom(email) + 1 
             };
-            this.messageInput = new TextField
+
+            var messageInput = new TextField
             {
-                X = Pos.Right(this.message) + 1,
-                Y = Pos.Bottom(this.email) + 1,
+                X = Pos.Right(message) + 1,
+                Y = Pos.Bottom(email) + 1,
                 Width = Dim.Fill() - 30,
             };
 
-            this.submitButton = new Button
+            var submitButton = new Button
             {
                 Text = "Submit",
-                Y = Pos.Bottom (this.message) + 1,
+                Y = Pos.Bottom (message) + 1,
 
                 // center the login button horizontally
                 X = Pos.Center (),
                 IsDefault = true
             };
 
-            this.submitButton.Accepting += (s, e) =>
+            submitButton.Accepting += (s, e) =>
             {
                 // Handle the submit button click event
-                string name = this.nameInput.Text.ToString();
-                string email = this.emailInput.Text.ToString();
-                string message = this.messageInput.Text.ToString();
+                string name = nameInput.Text.ToString();
+                string email = emailInput.Text.ToString();
+                string message = messageInput.Text.ToString();
 
                 // For demonstration, just show a message box
                 MessageBox.Query(50, 10, "Submitted", $"Name: {name}\nEmail: {email}\nMessage: {message}", "OK");
             };
 
             this.Add (
-                this.label, this.name, this.nameInput, 
-                this.email, this.emailInput, this.messageInput, 
-                this.message, this.submitButton
+                this.label, name, nameInput, 
+                email, emailInput, messageInput, 
+                message, submitButton
             );
+            this.CanFocus = true;
             
         }
     }
